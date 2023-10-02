13854712295d061458b345e48c8575187fffca21
fogiznt <85696249+fogiznt@users.noreply.github.com>
2023-10-02 19:10:22 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ cat >/etc/shadowsocks-libev/config.json <<FOE
     "mode":"tcp_and_udp",
     "server_port":8388,
     "local_port":1080,
-    "password":\"$password\",
+    "password":"$password",
     "timeout":86400,
     "method":"chacha20-ietf-poly1305"
 }
